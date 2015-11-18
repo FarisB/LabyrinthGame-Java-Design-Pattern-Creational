d750b04dc50bb526e3a1fbde3ba8fe938db2e63d
FarisB <faris26000@gmail.com>
2015-11-18 19:39:46 +0100

    MakeRAI fonctionnel

--- src/fr/iutvalence/info/dut/m3105/labyrinthGame/RobotArtificialIntelligence.java
+++ src/fr/iutvalence/info/dut/m3105/labyrinthGame/RobotArtificialIntelligence.java
@@ -1,6 +1,6 @@
 package fr.iutvalence.info.dut.m3105.labyrinthGame;
 
-public interface RobotArtificialIntelligence
+public abstract class RobotArtificialIntelligence 
 {
 	/**
 	 * callback method used to get the action the robot wish to perform
@@ -16,4 +16,22 @@ public interface RobotArtificialIntelligence
 	 * callback method used  to notify move result (if robot last action type was MOVE_FORWARD)
 	 */
 	public abstract void notifyMoveResult(boolean hasMoved);
+	
+	
+	public static RobotArtificialIntelligence makeRAI(String robot){
+		
+		switch(robot){
+			case "DBAI":{
+				DumbBotArtificialIntelligence DBAI = new DumbBotArtificialIntelligence();
+				return DBAI;
+			}
+			case "LDBAI":{
+				LesserDumbBotArtificialIntelligence LDBAI = new LesserDumbBotArtificialIntelligence();
+				return LDBAI;
+			}
+				
+		}
+		return null;
+	}
+	
 }
